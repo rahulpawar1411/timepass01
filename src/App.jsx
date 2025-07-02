d82858a5498e450d55d52ab94191e52a845e30a0
Rahul <rpawar141199@gmail.com>
2025-07-02 17:19:44 +0530

    errorHandle

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     <Route path='/' element={<Home/>} />
     <Route path='/about' element={<About/>} />
     <Route path='/help' element={<Help/>} />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 
   </>
